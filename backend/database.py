cf484a1a0a252a5db87deb128ccacac623f0cb44
Andres <andres@Mac-mini-Andres.local>
2026-02-11 21:29:22 +0300

    Fix: Postgres connection args

--- backend/database.py
+++ backend/database.py
@@ -4,7 +4,11 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./tz_generator.db")
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+connect_args = {}
+if "sqlite" in DATABASE_URL:
+    connect_args = {"check_same_thread": False}
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
